819158be911a2d3fbfac625719a696037f1dba0b
SerTor2 <sertor27@gmail.com>
2020-01-14 20:20:41 +0100

    Primera prueba de creación de un plano modificable

---
+++ IdleRPG/Assets/Scripts/Procedural/GeometryProcedural.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeometryProcedural : MonoBehaviour
+{
+    [HideInInspector]
+    private static int xSize = 0, zSize = 0;
+    private float density = 0;
+    private Mesh myMesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+    public void PassAllNums(int _xSize, int _zSize, float _density)
+    {
+        xSize = _xSize;
+        zSize = _zSize;
+        density = _density;
+    }
+
+    public void CreateAllVertexs()//Crea los vertices de la mesh dependiendo de los parametros dados
+    {
+        if (myMesh != null)
+        {
+            myMesh = null;
+            GetComponent<MeshFilter>().mesh = null;
+            vertices = null;
+            triangles = null;
+        }
+        myMesh = new Mesh();
+
+
+        int l_numOfVertex = (xSize + 1) * (zSize + 1);
+        vertices = new Vector3[l_numOfVertex];
+
+
+        for (int k = 0, num = 0; k <= zSize; k++)
+        {
+            for (int i = 0; i <= xSize; i++)
+            {
+                vertices[num] = new Vector3(i, 0, k);
+                num++;
+            }
+        }
+
+        myMesh.vertices = vertices;
+        CreateAllTriangles();
+    }
+
+    private void CreateAllTriangles()
+    {
+        triangles = new int[xSize * zSize * 6];
+        int l_vert = 0;
+        int l_tris = 0;
+
+        for (int k = 0; k < zSize; k++)
+        {
+            for (int i = 0; i < xSize; i++)
+            {
+                triangles[l_tris + 0] = l_vert + 0;
+                triangles[l_tris + 1] = l_vert + xSize + 1;
+                triangles[l_tris + 2] = l_vert + 1;
+                triangles[l_tris + 3] = l_vert + 1;
+                triangles[l_tris + 4] = l_vert + xSize + 1;
+                triangles[l_tris + 5] = l_vert + xSize + 2;
+
+
+                l_vert++;
+                l_tris += 6;
+            }
+        }
+        myMesh.triangles = triangles;
+        myMesh.RecalculateNormals();
+        myMesh.name = "My Mesh";
+
+        GetComponent<MeshFilter>().mesh = myMesh;
+
+    }
+}
---
+++ IdleRPG/Assets/Scripts/Procedural/GeometryProceduralEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GeometryProcedural))]
+public class GeometryProceduralEditor : Editor
+{
+    private static float xSize = 0, zSize = 0, density = 0;
+    private static Mesh myMesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+    public override void OnInspectorGUI()//La función predeterminada del editor
+    {
+        base.OnInspectorGUI();
+        xSize = EditorGUILayout.Slider(xSize, 0.1f, 10f);
+        zSize = EditorGUILayout.Slider(zSize, 0.1f, 10f);
+        density = EditorGUILayout.Slider(density, 0.01f, 1f);
+
+        GeometryProcedural l_geometryP = (GeometryProcedural)target;
+
+        if (GUILayout.Button("Create Mesh"))
+        {
+            l_geometryP.PassAllNums(Mathf.FloorToInt(xSize), Mathf.FloorToInt(zSize), density);
+            l_geometryP.CreateAllVertexs();
+        }
+        
+
+    }
+}
