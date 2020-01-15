55e75a5d38d8a1bca2a6dd4b33ee68c31929f312
ViodDevelopment <viodDevelopment@gmail.com>
2020-01-15 12:50:10 +0100

    Etapas
    prueba movil

--- IdleRPG/Assets/Scripts/Etapas/Etapas.cs
+++ IdleRPG/Assets/Scripts/Etapas/Etapas.cs
@@ -1,35 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class etapas : MonoBehaviour
+public class Etapas : MonoBehaviour
 {
     #region Variables
     public int Etapa;
     public int Fase;
     public int VidaTotal;
-    //public int Damage;
+    public int VidaEnemigos;
     public int Enemigos;
-    public bool Etapa10;
+    public Button m_Avance;
+    public Button m_AvanzarFase;
+    public Text VerFase;
     #endregion
 
     //fase y etapa
     // Start is called before the first frame update
     void Start()
     {
-       
+        VidaTotal = 5;
+        Enemigos = 10;
+        Fase = 0;
+        m_Avance.onClick.AddListener(AvanceFase);
+        m_AvanzarFase.onClick.AddListener(PasarFase);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P)) //sustituir la variable del if por el boton 
-        {
-            Fase = 10;
-            //parar todo lo que haya en la escena y pasar a la fase 10
-        }
         ComprobarFasesEtapa();
-      
+        VerFase.text = "Fase: " + Fase + " etapa:" + Etapa;
 
     }
     private void ComprobarFasesEtapa()
@@ -39,21 +41,28 @@ public class etapas : MonoBehaviour
             Fase = 0;
             VidaTotal = 5;
         }
-        else if (Enemigos == 0 && Fase!=10)
+        else if (Enemigos == 0 && Fase != 10)
         {
             Fase++;
-        }else if (Enemigos == 0 && Fase==10)
+            Enemigos = 10;
+            VidaEnemigos = 10 + (5 * Fase);
+
+        }
+        else if (Enemigos == 0 && Fase == 10)
         {
             Fase = 0;
             Etapa++;
+            Enemigos = 1;
         }
-        
-    }
-    
 
-    /*
-    private void ScriptDeCosas(int _variable)//lo que hace
+    }
+    private void AvanceFase() // avanza directamente a la fase 10, se debera detener la fase en la que esta e iniciar la 10
     {
-        l_variableDentro
-    }*/
+        Fase = 10;
+    }
+    private void PasarFase()
+    {
+        Enemigos = 0;
+    }
+
 }
