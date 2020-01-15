6144d64bba7b50e72e0da24e210a6caa43781386
SerTor2 <sertor27@gmail.com>
2020-01-15 16:24:54 +0100

    progreso procedural

--- IdleRPG/Assets/Scripts/Procedural/GeometryProcedural.cs
+++ IdleRPG/Assets/Scripts/Procedural/GeometryProcedural.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class GeometryProcedural : MonoBehaviour
 {
     [HideInInspector]
-    private static int xSize = 0, zSize = 0;
-    private float density = 0;
+    private static int sizeX = 0, sizeZ = 0;
+    private static int density = 0;
     private Mesh myMesh;
     private Vector3[] vertices;
     private int[] triangles;
-    public void PassAllNums(int _xSize, int _zSize, float _density)
+    public void PassAllNums(int _xSize, int _zSize, int _density)
     {
-        xSize = _xSize;
-        zSize = _zSize;
+        sizeX = _xSize;
+        sizeZ = _zSize;
         density = _density;
     }
 
@@ -28,46 +28,40 @@ public class GeometryProcedural : MonoBehaviour
         }
         myMesh = new Mesh();
 
-
-        int l_numOfVertex = (xSize + 1) * (zSize + 1);
+        sizeX = sizeX * density;
+        sizeZ = sizeZ * density;
+        int l_numOfVertex = Mathf.Abs((sizeX + 1) * (sizeZ + 1));
         vertices = new Vector3[l_numOfVertex];
 
-
-        for (int k = 0, num = 0; k <= zSize; k++)
+        for (int l_z = 0, num = 0; l_z <= sizeZ; l_z++)
         {
-            for (int i = 0; i <= xSize; i++)
+            for (int l_x = 0; l_x <= sizeX; l_x++, num++)
             {
-                vertices[num] = new Vector3(i, 0, k);
-                num++;
+                vertices[num] = new Vector3((float)l_x / density, 0, (float)l_z / density);
             }
         }
-
         myMesh.vertices = vertices;
         CreateAllTriangles();
     }
 
     private void CreateAllTriangles()
     {
-        triangles = new int[xSize * zSize * 6];
-        int l_vert = 0;
-        int l_tris = 0;
+        triangles = new int[sizeX * sizeZ * 6];
 
-        for (int k = 0; k < zSize; k++)
+        for (int tris = 0, l_indexZ = 0, y = 0; y < sizeZ; y++, l_indexZ++)//Cambiar por mio en un futuro
         {
-            for (int i = 0; i < xSize; i++)
+            for (int l_indexX = 0; l_indexX < sizeX; l_indexX++, tris += 6, l_indexZ++) //cambiar por mio en un futuro
             {
-                triangles[l_tris + 0] = l_vert + 0;
-                triangles[l_tris + 1] = l_vert + xSize + 1;
-                triangles[l_tris + 2] = l_vert + 1;
-                triangles[l_tris + 3] = l_vert + 1;
-                triangles[l_tris + 4] = l_vert + xSize + 1;
-                triangles[l_tris + 5] = l_vert + xSize + 2;
-
-
-                l_vert++;
-                l_tris += 6;
+                triangles[tris] = l_indexZ;
+                triangles[tris + 1] = l_indexZ + sizeX + 1;
+                triangles[tris + 2] = l_indexZ + 1;
+                triangles[tris + 3] = l_indexZ + 1;
+                triangles[tris + 4] = l_indexZ + sizeX + 1;
+                triangles[tris + 1] = l_indexZ + sizeX + 1;
+                triangles[tris + 5] = l_indexZ + sizeX + 2;
             }
         }
+        print("cantidad de vertices: " + vertices.Length + " cantidad de tris: " + triangles.Length);
         myMesh.triangles = triangles;
         myMesh.RecalculateNormals();
         myMesh.name = "My Mesh";
