34e2717323c93fa6dcaa43aeebeea18eed92034b
ViodDevelopment <viodDevelopment@gmail.com>
2020-01-15 16:07:33 +0100

    CAMARA
    pruebas

---
+++ IdleRPG/Assets/Scripts/Camara/CameraEspecial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEspecial : MonoBehaviour
+{
+    private Collider ObjetoGolpeado;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100) && hit.distance <= 10)
+        {
+            Vector3 PosicionPrueba = new Vector3(0f, 0f, 10f);
+            //print("La distacia es " + hit.distance);
+            ObjetoGolpeado = hit.collider;
+            print(ObjetoGolpeado.transform.position.x);
+            ObjetoGolpeado.transform.position = PosicionPrueba;
+            //ObjetoGolpeado.attachedRigidbody.MovePosition(PosicionPrueba);
+
+        }
+        Debug.DrawLine(ray.origin, hit.point);
+
+    }
+}
