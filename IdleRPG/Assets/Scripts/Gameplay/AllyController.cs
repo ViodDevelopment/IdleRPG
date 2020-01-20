6e76c614fecb7630b7e5f37acdfb4d6e8eb5757b
viodart <viodart@gmail.com>
2020-01-20 11:44:29 +0100

    gitignore y principio de combate

---
+++ IdleRPG/Assets/Scripts/Gameplay/AllyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyController : MonoBehaviour
+{
+    enum States { IDLE, MOVING, FIGHTING, DEAD };
+
+    #region Combat
+    public int health;
+    public bool alive = true;
+
+    public int hitsTaken = 0, hitsDealt = 0;
+
+    public float range;
+    public List<GameObject> enemiesInRange;
+
+    public float basicAttackDmg;
+
+
+    public void RecieveDmg(int dmg)
+    {
+        health -= dmg;
+
+        if (health <= 0) alive = false;
+    }
+
+
+
+    #endregion
+
+    public void UpdateAlly()
+    {
+
+    }
+
+
+}
