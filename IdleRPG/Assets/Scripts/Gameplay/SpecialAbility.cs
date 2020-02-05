4273f2637fe65ff85d2a3f354664b32030e9c3db
viodart <viodart@gmail.com>
2020-02-05 12:33:03 +0100

    Habilidades

---
+++ IdleRPG/Assets/Scripts/Gameplay/SpecialAbility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialAbility : MonoBehaviour
+{
+    public string name;
+    public float cooldown;
+    public int dmg;
+    public int energyRequired;
+    
+
+    public SpecialAbility(string _name , float _cooldown, int _dmg, int _energy)
+    {
+        name = _name;
+        cooldown = _cooldown;
+        dmg = _dmg;
+        energyRequired = _energy;
+    }
+    public virtual void UseAb()
+    {
+        Debug.Log("Hab used");
+    }
+
+}
